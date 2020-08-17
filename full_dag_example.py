4e948b493eba0fed64d0f87914073f2bbb64c263
Gil Tober <giltober@gmail.com>
2020-08-17 18:17:05 +0300

    Final full example

--- full_dag_example.py
+++ full_dag_example.py
@@ -34,33 +34,35 @@ def pull_exchange_rate():
     rate_date = today - delta
 
     req = requests.get(f"http://data.fixer.io/api/{rate_date}?access_key={str(Variable.get('EXCHANGE_KEY'))}&base=EUR")
+
     if req.json()['success']:
         con = create_engine(str(Variable.get('AIRFLOW_CONN_POSTGRES_PROD')))
         usd_currency = req.json()['rates']['USD']
+
         df = pd.DataFrame(req.json()).drop(['success', 'base', 'timestamp', 'historical'], axis=1).reset_index().rename(
             columns={'index': 'code', 'rates': 'rate'})
+
         df['rate'] = df['rate'].apply(to_usd, base=usd_currency)
         df = df[['code', 'rate', 'date']].astype({'date': 'datetime64'})
+
         df.to_sql('usd_exchange_rates', con=con, schema='prod', if_exists='append', index=False, chunksize=200,
                   method='multi')
 
 
-create_table = PostgresOperator(sql='sql.sql', postgres_conn_id='postgres_prod', autocommit=True,
-                                database='postgres',
+create_table = PostgresOperator(sql='sql.sql', postgres_conn_id='postgres_prod', autocommit=True, database='postgres',
                                 task_id='create_table', dag=dag)
 
 pull_exchange = PythonOperator(python_callable=pull_exchange_rate, task_id='pull_exchange', dag=dag)
 
 on_fail_telegram_message = TelegramOperator(bot_token=str(Variable.get('TELEGRAM_TOKEN')),
                                             send_to=Variable.get('TELEGRAM_USER'),
-                                            msg=f'{dt.datetime.now().replace(microsecond=0)}: table creations failed',
+                                            msg=f'{dt.datetime.now().replace(microsecond=0)}: API call failed',
                                             task_id='on_fail_telegram_message', dag=dag,
                                             trigger_rule='all_failed')
 
 on_success_telegram_message = TelegramOperator(bot_token=str(Variable.get('TELEGRAM_TOKEN')),
                                                send_to=Variable.get('TELEGRAM_USER'),
-                                               msg=f'{dt.datetime.now().replace(microsecond=0)}: table created '
-                                                   f'successfully',
+                                               msg=f'{dt.datetime.now().replace(microsecond=0)}: API call successful',
                                                task_id='on_success_telegram_message', dag=dag,
                                                trigger_rule='all_success')
 
