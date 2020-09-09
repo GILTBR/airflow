ff354b5603f440fd24357361424da5e0d2fe59b9
Gil Tober <giltober@gmail.com>
2020-09-09 17:54:41 +0300

    Refactored sql_version_control_v2

--- dags/sql_version_control_v2.py
+++ dags/sql_version_control_v2.py
@@ -1,6 +1,7 @@
 import datetime as dt
 import os
 
+import pendulum
 from airflow import DAG, settings
 from airflow.models import Variable, Connection
 from airflow.operators.bash_operator import BashOperator
@@ -12,61 +13,75 @@ from airflow.utils.dates import days_ago
 # Main DAG info
 DAG_NAME = 'sql_version_control_v2'
 SCHEDULE = None
-DESCRIPTION = 'This DAG is used to control versioning sql functions and procedures on a giving database. ' \
-              'Version 2: several DBs and 2 Folders that each contains several .sql files.'
+DESCRIPTION = 'Version 2: Dynamically create DAGs based on the number of connections that meat the criteria'
 
 # Constant variables
 VERSION = DAG_NAME.split('_')[-1]
 SQL_MAIN_FOLDER = str(Variable.get('SQL_FOLDER_PATH'))
-SQL_DELETE_FOLDER = f'{SQL_MAIN_FOLDER}/{VERSION}/delete'
-SQL_CREATE_FOLDER = f'{SQL_MAIN_FOLDER}/{VERSION}/create'
+SQL_FUNCTIONS_FOLDER = f'{SQL_MAIN_FOLDER}/{VERSION}'
+LOCAL_TZ = pendulum.timezone('Asia/Jerusalem')
 
-bash_command = f'cd {SQL_MAIN_FOLDER}; git pull'
+# Default DAG arguments
+default_args = {'owner': 'Gil Tober', 'start_date': days_ago(2), 'depends_on_past': False,
+                'email': ['giltober@gmail.com'], 'email_on_failure': False}
 
 
+def on_success_callback_telegram(context):
+    message = f"\U00002705 DAG successful!\nDAG: {context.get('task_instance').dag_id}\nExecution Time: " \
+              f"{context.get('execution_date').replace(microsecond=0, tzinfo=LOCAL_TZ)}\nLog URL:\n" \
+              f"{context.get('task_instance').log_url}"
+    success_alert = TelegramOperator(telegram_conn_id='telegram_conn_id', task_id='telegram_success', message=message)
+    return success_alert.execute(context=context)
+
+
+def on_failure_callback_telegram(context):
+    message = f"\U0000274C Task Failed!\nDAG: {context.get('task_instance').dag_id}\nTask: " \
+              f"{context.get('task_instance').task_id}\nExecution Time: " \
+              f"{context.get('execution_date').replace(microsecond=0, tzinfo=LOCAL_TZ)}\nLog URL:\n" \
+              f"{context.get('task_instance').log_url}"
+    failed_alert = TelegramOperator(telegram_conn_id='telegram_conn_id', task_id='telegram_failed', message=message)
+    return failed_alert.execute(context=context)
+
+
+# Main DAG creation
 def create_dag(f_dag_id, f_schedule, f_default_args, f_conn_id):
-    with DAG(dag_id=f_dag_id, description=DESCRIPTION, default_view='graph', default_args=f_default_args,
+    with DAG(dag_id=f_dag_id, description=DESCRIPTION, default_args=f_default_args,
              template_searchpath=f'{SQL_MAIN_FOLDER}', schedule_interval=f_schedule,
              dagrun_timeout=dt.timedelta(minutes=60),
-             tags=['git', 'sql']) as dag:
+             on_failure_callback=on_failure_callback_telegram, on_success_callback=on_success_callback_telegram) as dag:
+        # On failure send telegram message
+        git_pull = BashOperator(task_id='git_pull', bash_command=f'cd {SQL_MAIN_FOLDER}; git pull',
+                                on_failure_callback=on_failure_callback_telegram)
 
-        git_pull = BashOperator(task_id='git_pull', bash_command=bash_command)
+        dummy_start = DummyOperator(task_id='dummy_start')
+        dummy_end = DummyOperator(task_id='dummy_end')
 
-        dummy1 = DummyOperator(task_id='dummy1')
-        dummy2 = DummyOperator(task_id='dummy2')
+        git_pull >> dummy_start
 
-        for file in os.listdir(SQL_DELETE_FOLDER):
+        # Loop over the SQL files folder to create tasks
+        # On failure send telegram message
+        for file in os.listdir(SQL_FUNCTIONS_FOLDER):
             file_name = file.split('.')[0]
-            delete_sql = PostgresOperator(task_id=f'delete_sql_{file_name}', postgres_conn_id=str(f_conn_id[0]),
-                                          sql=f'{VERSION}/delete/{file}', autocommit=True)
-            git_pull >> delete_sql >> dummy1
+            sql_function = PostgresOperator(task_id=f'sql_{file_name}', postgres_conn_id=str(f_conn_id[0]),
+                                            sql=f'{VERSION}/{file}', autocommit=True)
+            dummy_start >> sql_function >> dummy_end
 
-        for file in os.listdir(SQL_CREATE_FOLDER):
-            file_name = file.split('.')[0]
-            create_sql = PostgresOperator(task_id=f'create_sql_{file_name}', postgres_conn_id=str(f_conn_id[0]),
-                                          sql=f'{VERSION}/create/{file}', autocommit=True)
-            dummy1 >> create_sql >> dummy2
-
-        on_fail_telegram_message = TelegramOperator(telegram_conn_id='telegram_conn_id',
-                                                    message=f'{dt.datetime.now().replace(microsecond=0)}: {DAG_NAME} failed'
-                                                    , task_id='on_fail_telegram_message', trigger_rule='all_failed')
-        on_success_telegram_message = TelegramOperator(telegram_conn_id='telegram_conn_id',
-                                                       message=f'{dt.datetime.now().replace(microsecond=0)}: {DAG_NAME} '
-                                                               f'successful',
-                                                       task_id='on_success_telegram_message',
-                                                       trigger_rule='all_success')
-
-    dummy2 >> on_fail_telegram_message
-    dummy2 >> on_success_telegram_message
     return dag
 
 
-session = settings.Session()
-conns = (session.query(Connection.conn_id).filter(Connection.conn_id.like('db_%')).all())
+def dynamic_dag_creation():
+    # Create session to Airflow DB to get connections available
+    global default_args
+    session = settings.Session()
+    connections = (session.query(Connection.conn_id).filter(Connection.conn_id.like('db_%')).all())
 
-for db_conn in conns:
-    dag_id = f'{DAG_NAME}_{db_conn[0]}'
-    default_args = {'owner': 'Gil Tober', 'start_date': days_ago(2), 'depends_on_past': False,
-                    'email': ['giltober@gmail.com'], 'email_on_failure': False}
-    schedule = SCHEDULE
-    globals()[dag_id] = create_dag(dag_id, schedule, default_args, db_conn)
+    # dynamically create DAGs based on connections
+    for db_conn in connections:
+        dag_id = f'{DAG_NAME}_{db_conn[0]}'
+        default_args = default_args
+        schedule = SCHEDULE
+        globals()[dag_id] = create_dag(dag_id, schedule, default_args, db_conn)
+
+
+if __name__ == "__main__":
+    dynamic_dag_creation()
