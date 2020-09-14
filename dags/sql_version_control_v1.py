81d1bf339c2439f980f4fe3c76b17482732ee03a
Gil Tober <giltober@gmail.com>
2020-09-14 18:58:27 +0300

    testing

--- dags/sql_version_control_v1.py
+++ dags/sql_version_control_v1.py
@@ -50,9 +50,10 @@ def on_failure_callback_telegram(context):
 # Main DAG creation
 with DAG(dag_id=DAG_NAME, description=DESCRIPTION, default_args=default_args, template_searchpath=f'{SQL_MAIN_FOLDER}',
          schedule_interval=SCHEDULE, dagrun_timeout=dt.timedelta(minutes=60),
-         on_failure_callback=on_failure_callback_telegram, on_success_callback=on_success_callback_telegram) as dag:
+         on_success_callback=on_success_callback_telegram) as dag:
     # On failure send telegram message
-    git_pull = BashOperator(task_id='git_pull', bash_command=f'cd {SQL_MAIN_FOLDER}; git pull')
+    git_pull = BashOperator(task_id='git_pull', bash_command=f'cd {SQL_MAIN_FOLDER}; git pull',
+                            on_failure_callback=on_failure_callback_telegram)
 
     dummy_start = DummyOperator(task_id='dummy_start')
     dummy_end = DummyOperator(task_id='dummy_end')
@@ -64,7 +65,8 @@ with DAG(dag_id=DAG_NAME, description=DESCRIPTION, default_args=default_args, te
     for file in os.listdir(SQL_FUNCTIONS_FOLDER):
         file_name = file.split('.')[0]
         sql_function = PostgresOperator(task_id=f'sql_{file_name}', postgres_conn_id='postgres_prod',
-                                        sql=f'{VERSION}/{file}', autocommit=True)
+                                        sql=f'{VERSION}/{file}', autocommit=True,
+                                        on_failure_callback=on_failure_callback_telegram)
 
         dummy_start >> sql_function >> dummy_end
 
